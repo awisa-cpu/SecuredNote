c093ea8615b7890964257a89a9018b3b491d4ee8
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-07-24 00:06:38 +0100

    initialized the app for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:samsung_note/services/authentication/auth_services.dart';
 import 'package:samsung_note/utilities/routes/route.dart';
-import 'package:samsung_note/views/home.dart';
+import 'package:samsung_note/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +14,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Samsung_note',
+      title: 'secured_note',
       onGenerateRoute: (settings) => generateRoutes(settings),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.fromFirebase().initializeApp(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return const LoginView();
+
+          case ConnectionState.waiting:
+            return const Loader(
+              text: 'In progress ...',
+            );
+
+          default:
+            return const Loader(
+              text: 'Waiting ...',
+            );
+        }
+      },
+    );
+  }
+}
+
+class Loader extends StatelessWidget {
+  final String? text;
+  const Loader({super.key, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text ?? ''),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
