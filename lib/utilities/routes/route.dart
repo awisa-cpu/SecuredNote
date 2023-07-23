f86906ef628f8c0d904f345b5b3343dff3f9830f
I_AM_AWISA_DESTINY <destinyawisa@gmail.com>
2023-07-24 00:08:44 +0100

    added two new routes

--- lib/utilities/routes/route.dart
+++ lib/utilities/routes/route.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:samsung_note/utilities/routes/routes_constants.dart';
-import 'package:samsung_note/views/home.dart';
-import 'package:samsung_note/views/new_note.dart';
+import 'package:samsung_note/views/notes/notes_view.dart';
+import 'package:samsung_note/views/login_view.dart';
+import 'package:samsung_note/views/notes/new_note.dart';
+import 'package:samsung_note/views/register_view.dart';
 
 Route generateRoutes(RouteSettings settings) {
   return MaterialPageRoute(
@@ -12,7 +14,13 @@ Route generateRoutes(RouteSettings settings) {
           return const NewNote();
 
         case homeRoute:
-          return const Home();
+          return const NotesView();
+
+        case loginRoute:
+          return const LoginView();
+
+        case registerRoute:
+          return const RegisterView();
 
         default:
           return const Scaffold(
